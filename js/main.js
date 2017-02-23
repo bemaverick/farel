9d074e139938a704598605831802e9c58b116aea
Serhii Morykon <sergomen153@gmail.com>
2017-02-23 21:47:00 +0200

    add scrolling from button contacts and fixed header block

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-		var buttonWrite=document.querySelector(".scroll-down");
-			var body=document.querySelector("body");
-
-			buttonWrite.addEventListener("click", function(event){
+var buttonDown=document.querySelector(".scroll-down");
+var body=document.querySelector("body");
+		buttonDown.addEventListener("click", function(event){
     			event.preventDefault();
     			var scrolledValue=body.scrollTop;
     			var top = scrolledValue;
@@ -10,10 +9,27 @@
 					top = top + 10;
 					console.log(top);
 				    window.scrollTo(scrolledValue, top);	    
-					if (top > 600+ scrolledValue) {
+					if (top > 550 + scrolledValue) {
 					 	    clearInterval(scr);
 					    }
 					}, 5);
     		});
 
+
+var contact=document.querySelector(".contact-link"); 
+	contact.addEventListener("click", function(event) {
+		event.preventDefault();	
+		var scrolledValue=body.scrollTop;
+    	var top = scrolledValue;
+    		var scr = setInterval(function () {	
+				top = top + 20;
+				console.log(top);
+				window.scrollTo(scrolledValue, top);	    
+				if (top > 3150) {
+			    clearInterval(scr);
+				}
+			}, 5);
+
+	});
+	
 			
